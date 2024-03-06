dbd5ca566f216cf2bea421a873a8110fb7fa170d
kaxna <162370892+kaxna@users.noreply.github.com>
2024-03-06 11:39:24 +0100

    Update exercise_1.py

--- lab1-functions/exercise_1.py
+++ lab1-functions/exercise_1.py
@@ -1,1 +1,24 @@
 ## place for exercise 1
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# chcemy to: cos(frequency * 2pi * t)
+
+def plot_cosinus():
+  t = np.arange(0.0, 2.0, 0.01)
+  s = np.cos(5 * 2*np.pi * t)
+
+  fig, ax = plt.subplots()
+  ax.plot(t, s, label='base')
+  ax.plot(t, s/2 + 2.5, label='s/2 + 2.5')
+  ax.plot(t, s*2 - 3.5, label='s*2 - 3.5')
+
+  ax.set(xlabel='t', ylabel='s',
+        title='Pierwszy wykres')
+
+  ax.grid()
+  ax.legend(loc= 'lower right')
+  plt.show()
+
+plot_cosinus()
